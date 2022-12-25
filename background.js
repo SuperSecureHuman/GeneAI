d46d925a08c51b1aa12f200b322c8c23b0406b23
Eswar Divi <eswar.divi.902@gmail.com>
2022-12-25 21:13:06 +0530

    added Summarizer

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ chrome.contextMenus.create({
 
 // "https://api-inference.huggingface.co/models/gpt2",
 
-async function query(data) {
+async function Generator(data) {
   const response = await fetch(
     "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6B",
     {
@@ -19,6 +19,23 @@ async function query(data) {
       },
       method: "POST",
       body: JSON.stringify(data),
+    
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+async function Summarizer(data) {
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    
     }
   );
   const result = await response.json();
@@ -29,7 +46,7 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId == "Generate") {
     console.log("Input");
     console.log(info.selectionText);
-    query({ inputs: info.selectionText }).then((response) => {
+    Generator({ inputs: info.selectionText }).then((response) => {
       console.log("Generated From Background.js");
       console.log(response[0].generated_text);
     });
@@ -39,10 +56,20 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   let text = request.text;
   let dropdown = request.dropdown;
-  query({ inputs: text }).then((response) => {
-    let result = response[0].generated_text;
-    sendResponse({ result: result });
-    console.log(response[0].generated_text);
-  });
+  if (dropdown == "generate"){
+
+    Generator({ inputs: text }).then((response) => {
+      let result = response[0].generated_text;
+      sendResponse({ result: result });
+      console.log(response[0].generated_text);
+    });
+  }
+  else if (dropdown == "Summarizer"){
+    Summarizer({ inputs: text }).then((response) => {
+      let result = response[0].summary_text;
+      sendResponse({ result: result });
+      console.log(response[0].summary_text);
+    });
+  }
   return true;
 });
--- popup.js
+++ popup.js
@@ -3,10 +3,6 @@ document.getElementById("Gen-generate").addEventListener("click", () => {
     console.log("Generate Button Clicked");
     let text = document.getElementById("Gen-text").value;
     let dropdown = document.getElementById("Gen-dropdown").value;
-    if (dropdown == "Summarizer") {
-        document.getElementById("Gen-result").value = "Coming Soon..... ";
-        return;
-    }
     if (text.length > 0) {
         chrome.runtime.sendMessage({ text: text, dropdown: dropdown }, (response) => {
             console.log(response);
