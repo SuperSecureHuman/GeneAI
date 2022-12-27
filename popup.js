b644dc63037fe6d519238b6d207f205ea4ec3707
Eswar Divi <eswar.divi.902@gmail.com>
2022-12-27 15:51:39 +0530

    removed undo,revamped ui

--- popup.js
+++ popup.js
@@ -1,20 +1,41 @@
-let temp=[];
-
+let result_history=[""];
 
 document.getElementById("Gen-generate").addEventListener("click", () => {
     console.log("Generate Button Clicked");
     let text = document.getElementById("Gen-text").value;
     let dropdown = document.getElementById("Gen-dropdown").value;
+    if(result_history.length==1){
+        result_history.push(text);
+    }
+    text=result_history[result_history.length-1];
     if (dropdown=="generate"){
 
         if (text.length > 0) {
             chrome.runtime.sendMessage({ text: text, dropdown: dropdown }, (response) => {
                 console.log(response);
                 let result = response.result;
-                document.getElementById("Gen-result").value = result;
-                temp.push(result);
+                result_history.push(result);
+
+                let i=0;
+                let nspeed = 10;
+                function typeWriter() {
+                    text=response.result;
+
+                    if (i < text.length) {
+                        if(i<result_history[result_history.length-2].length){
+                            nspeed=0;
+                        }
+                        else{
+                            nspeed=10;
+                        }
+                        i++;
+                        document.getElementById("Gen-result").value = text.substring(0, i);
+                        setTimeout(typeWriter, nspeed);
+                    }
+                }
+
+                typeWriter();
                 console.log("Temp:"+text);
-                document.getElementById("Gen-text").value = result;
             });
         }
     }
@@ -41,20 +62,21 @@ document.getElementById("Gen-dropdown").addEventListener("change", () => {
 
 });
 
-document.getElementById("Undo").addEventListener("click", () => {
-    console.log("Undo Button Clicked");
-    // replace the text in the text box with the last text in the temp array
-    document.getElementById("Gen-text").value = temp[temp.length - 2];
-    // remove the last element from the temp array
-    temp.pop();
-    // clear the result text box
-    document.getElementById("Gen-result").value = "";
-});
+// document.getElementById("Undo").addEventListener("click", () => {
+//     console.log("Undo Button Clicked");
+//     // replace the text in the text box with the last text in the temp array
+//     document.getElementById("Gen-text").value = temp[temp.length - 2];
+//     // remove the last element from the temp array
+//     temp.pop();
+//     // clear the result text box
+//     document.getElementById("Gen-result").value = "";
+// });
 
 
 
 document.getElementById("Gen-clear").addEventListener("click", () => {
     document.getElementById("Gen-text").value = "";
     document.getElementById("Gen-result").value = "";
+    result_history=[""];
 }   
 );
